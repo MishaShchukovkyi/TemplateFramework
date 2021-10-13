647842b00f58430f059517f27c9b18a42f4812da
KYIV\Mykhailo_Shchukovsky <mishaenter2009@gmail.com>
2021-10-13 15:40:35 +0300

    Apitests

---
+++ ApiTests/ApiTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+
+namespace ApiTests
+{
+    [TestFixture]
+    public static class ApiTests
+    {
+        [Test]
+        public static void test()
+        {
+            var restClient = new RestClient();
+            var resRequest = new RestRequest("https://api.coinpaprika.com/v1/coins");
+
+            IRestResponse<List<Coooin>> response = restClient.Get<List<Coooin>>(resRequest);
+            var actives = response.Data.Where(a => a.is_active == true);
+
+
+
+        }
+
+
+        [Test]
+        public static void test2()
+        {
+            var restClient = new RestClient();
+            var resRequest = new RestRequest("https://api.coinpaprika.com/v1/coins/btc-bitcoin/exchanges");
+
+            IRestResponse<List<Root>> response = restClient.Get<List<Root>>(resRequest);
+
+            var a = response.Data[0].fiats[1].name;
+        }
+    }
+}
---
+++ ApiTests/Root.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ApiTests
+{
+    public class Fiat
+    {
+        public string name { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class Root
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public double adjusted_volume_24h_share { get; set; }
+        public List<Fiat> fiats { get; set; }
+    }
+}
